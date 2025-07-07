66d5b76ce58aa7f1abee63568fc63eeaa30c88c6
Mystic Research <mysticresearcher@gmail.com>
2025-07-07 16:51:31 +0200

    Add RAG from official guide of Nike company informaiton

---
+++ langchain-from-docs/secrets_langchain.py
@@ -0,0 +1,10 @@
+#sample keys
+
+
+LANGSMITH_TRACING="true"
+# LANGSMITH_API_KEY="lsv2_pt_09afe748901c45a9b2598c76c200015d_6b8c6f83dfbcdx" £dont remember
+LANGSMITH_API_KEY="lsv2_pt_29966194af234a4b8db426bb82f2611d_asdfasf" #  deepak25041gmail wali id
+LANGSMITH_PROJECT="chat-models-prompt-templates" # or any other project name
+OPENAI_API_KEY="sk-proj-K2vwGs9O-Hsef0skA3tCOJJQKK3mt8CVfZaSpzWqyTJJNDlYks9pwSMkZg6eEiN3Ax9asfdsfNegjQ8ZPz0qYZn7ShJaWMRmYzDm8-rWHylGaV6lNZN9AsPhcZxcIeZeciRLo_hqA3ixZkf8A"
+serpapi_key = "a9111e39d58e45af13ae884f8ce1567608887f4330b2dbasdfsdafd5ace"
+
